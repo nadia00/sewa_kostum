0157740c2ac913b48f8ac73d67a977aecda9b0ad
Nadia <nadiapermata96@gmail.com>
2018-03-28 11:11:07 +0700

    no message

---
+++ resources/views/login-jasa.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Login Penyewa</title>
+        
+    </head>
+    <body>
+        <h2>Masuk sebagai penyedia jasa</h2>
+        <form>
+            <input type="text" name="username">
+            <input type="password" name="password">
+            <button type="submit">Masuk</button>
+        </form>
+        
+        <br><hr>
+        <p>Masuk sebagai <a href="login-penyewa.blade.php">Penyewa?</a></p>
+    </body>
+</html>
---
+++ resources/views/login-penyewa.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Login Penyewa</title>
+        
+    </head>
+    <body>
+        <h2>Masuk sebagai penyewa</h2>
+        <form>
+            <input type="text" name="username">
+            <input type="password" name="password">
+            <button type="submit">Masuk</button>
+        </form>
+        
+        <br><hr>
+        <p>Masuk sebagai <a href="login-jasa.blade.php">pemilik jasa?</a></p>
+    </body>
+</html>
