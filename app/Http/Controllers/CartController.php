f0fd7759d6052b9bad0229924fcf44d25e4be5e2
NandaRahman <nandareus05@gmail.com>
2018-07-08 18:34:52 +0700

    Yup

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,70 +2,158 @@
 
 namespace App\Http\Controllers;
 
-use App\CartStorage;
-use Darryldecode\Cart\Cart;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CartController extends Controller{
 
     public function store(Request $request){
-//        $data = [
-//            "id"=>$request->id.$request->size_id,
-//            "name"=>$request->name,
-//            "qty"=>$request->qty,
-//            "price"=>$request->price,
-//            "options"=>[
-//                "id"=>$request->id,
-//                "id_shop"=>$request->id_shop,
-//                "id_product"=>$request->size_id,
-//                "size"=>$request->size_id,
-//                "image"=>$request->image,
-//            ]
-//        ];
-//        $user_id = auth()->user()->id;
-//        Cart::session($user_id)->add($data);
-
-        Cart::add(array(
+        $data = array(
             "id"=>$request->id.$request->size_id,
             "name"=>$request->name,
-            "qty"=>$request->qty,
+            "quantity"=>$request->qty,
             "price"=>$request->price,
-            "options"=>array(
+            "attributes"=>array(
                 "id"=>$request->id,
                 "id_shop"=>$request->id_shop,
                 "id_product"=>$request->size_id,
                 "size"=>$request->size_id,
                 "image"=>$request->image,
             )
-        ));
+        );
+        Cart::add($data);
+        var_dump(Cart::getContent());
     }
 
+
+
     public function delete($itemId){
-//        $user_id = auth()->user()->id;
         Cart::remove($itemId);
         return "OK";
     }
 
     public function destroy(){
-//        $user_id = auth()->user()->id;
         Cart::clear();
         return "OK";
     }
 
-    public function show(){
-//        $user_id = auth()->user()->id;
-        Cart::getContent();
-//        return
+    function size(){
+        $cartCollection = Cart::getTotalQuantity();
+        return (int)$cartCollection;
     }
 
-    function size(){
-        $cartCollection = Cart::getContent();
-        return (int)$cartCollection->count();
+    public function showOrder()
+    {
+        $carts = Cart::getContent();
+        if($this->size()<1){
+            return "
+                <div class='panel panel-danger text-center'>
+                    <div class='panel-heading'>No Cart Available</div>
+                </div>
+            ";
+        }else{
+            $data = "<table class=\"table table-striped cart-table\">
+                    <tr>
+                        <th></th>
+                        <th class='text-center'>Name</th>
+                        <th class='text-center'>Price</th>
+                        <th class='text-center'>Quantity</th>
+                        <th class='text-center'>Total</th>
+                    </tr>";
+
+            foreach ($carts as $cart){
+                $data.= "
+                    <tr>
+                        <td>
+                            <div class='row text-center'>
+                                <img style='max-width: 100px' class=\"img-responsive img-thumbnail\" src='".url('/').Storage::disk('local')->url("app/".$cart->attributes->image)."' alt='No Image'>
+                                </div>
+                        </td>
+                        <td>
+
+                            <div class='row text-center'>
+                                $cart->name
+                            </div>
+                        </td>
+                        <td class='text-center'>Rp. ".number_format($cart->price)."</td>
+                        <td class='text-center'>".$cart->quantity."</td>
+                        <td class='text-center'>Rp. ".number_format($cart->price*$cart->quantity)."</td>
+                    </tr>";
+            }
+            $data.= "</table>
+                    <div class=\"row\">
+                        <div class=\"col-md-6\"></div>
+                        <div class=\"col-md-6 \">
+                            <h3 class=\"border-bottom\">Subtotal:
+                            <span class=\"pull-right\">Rp. ".Cart::subTotal()."</span></h3>";
+            $data.="
+                        </div>
+                    </div>";
+            return $data;
+
+        }
     }
+
+    public function show()
+    {
+        $carts = Cart::getContent();
+        if($this->size()<1){
+            return "
+                <div class='panel panel-danger text-center'>
+                    <div class='panel-heading'>No Cart Available</div>
+                </div>
+            ";
+        }else{
+            $data = "<table class=\"table table-striped cart-table\">
+                    <tr>
+                        <th class='text-center'>Name</th>
+                        <th class='text-center'>Price</th>
+                        <th class='text-center'>Quantity</th>
+                        <th class='text-center'>Total</th>
+                        <th class=\"text-right\"></th>
+                    </tr>";
+
+            foreach ($carts as $cart){
+                $data.= "
+                    <tr>
+                        <td>
+                            <div class='row text-center'>
+                            <img style='max-width: 100px' class=\"img-responsive img-thumbnail\" src='".url('/').Storage::disk('local')->url("app/".$cart->attributes->image)."' alt='No Image'>
+                            </div>
+                            <div class='row text-center'>
+                                $cart->name
+                            </div>
+                        </td>
+                        <td class='text-center'>Rp. ".number_format($cart->price)."</td>
+                        <td class='text-center'>".$cart->quantity."</td>
+                        <td class='text-center'>Rp. ".number_format($cart->price*$cart->quantity)."</td>
+                        <td class='set-width-5 text-center'>
+                            <a href='#' class='btn btn-danger' onclick='deleteCart(\"$cart->id\")'><i class='fa fa-trash-o'></i></a>
+                        </td>
+                    </tr>";
+            }
+            $data.= "</table>
+                    <div class=\"row\">
+                        <div class=\"col-md-6\"></div>
+                        <div class=\"col-md-6 \">
+                            <h3 class=\"border-bottom\">Subtotal:
+                            <span class=\"pull-right\">Rp. ".Cart::getSubTotal()."</span></h3>
+                            <div class=\"padding-top pull-right\">";
+            if (Cart::getTotalQuantity() > 0){
+                $data.="<a href=".route('user.order-method')."><button class=\"btn btn-success\">Order <span class=\"fa fa-shopping-cart\"></span></button></a>
+                            or";
+            }
+            $data.="<a href=\"".url('/') ."\"><button class=\"btn btn-default\">Continue shopping</button></a>
+                            </div>
+                        </div>
+                    </div>";
+            return $data;
+
+        }
+    }
+
 }
 
 //use App\Shoppingcart;
